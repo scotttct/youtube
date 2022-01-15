c9b5decd89e3416ac2cf7613c7e0d3851a9f80be
ScottTomassetti <scott_t_ct@hotmail.com>
2022-01-15 06:29:06 -0500

    initial upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const apiKey = "AIzaSyB4dnBAJNoOmKup4Vaf6M2zlTXw_fTioKU"
+
+let today = new Date().getFullYear()
+document.getElementById('today').innerHTML = today
+
+  const showSearchResults = (data) => {
+
+    document.getElementById("search-results").innerHTML = ""
+
+    // loop over data.items
+    data.items.forEach(item => {
+        //check to see only videos and no undefined videos
+        if (item.kind != 'youtube#channel' && item.id.videoId != undefined) {
+          //I used let but const might work better here...
+            let videoId= item.id.videoId
+            htmlOutput =  `<div class="col text-center p-3">
+            <iframe width="400" height="300"
+             src="https://www.youtube.com/embed/${videoId}" 
+             title="YouTube video player" frameborder="0" allow="accelerometer;
+              autoplay; clipboard-write; encrypted-media; gyroscope; 
+              picture-in-picture" allowfullscreen></iframe>
+            </div>
+            `
+            //output to div and adding to list with +=
+            document.getElementById("search-results").innerHTML += htmlOutput
+        }
+     
+    })
+
+  }
+
+  const fetchDataFromYoutube = (searchStr) => {
+    
+    let url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&q=${searchStr}`
+
+    console.log('the url is', url)
+
+    fetch(url)
+    .then((res) => {
+      return res.json()
+    }).then((data) => {      
+      showSearchResults(data)
+    })
+  
+  }
+
+  document.getElementById("searchButton").addEventListener("click", function(e) {
+      e.preventDefault
+        console.log('the button was clicked')
+
+        let searchStr = document.getElementById('search-box').value
+
+        console.log('the search string is', searchStr)
+
+        fetchDataFromYoutube(searchStr)
+
+  })
